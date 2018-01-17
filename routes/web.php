ccc3a0a823ca3738bb7369cf48ca0909ee6cbe1e
willi-dev <willi.ilmukomputer@gmail.com>
2018-01-17 21:15:25 +0700

    add all files to repo

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('user/verify/{verification_code}', 'AuthController@verifyUser' );
+Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm' );
+Route::get('password/reset', 'Auth\PasswordController@reset' );
